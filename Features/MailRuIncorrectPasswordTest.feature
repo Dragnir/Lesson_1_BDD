5f103450923a578210275e04e5c4c9e0ee691818
Vadzim Kuryan <Vadzim_Kuryan@epam.com>
2023-02-24 18:35:51 +0400

    Add specflow

---
+++ Features/MailRuIncorrectPasswordTest.feature
@@ -0,0 +1,12 @@
+﻿Feature: MailRuIncorrectPasswordTest
+	In order to verify incorrect password
+	As a existing user
+	I want be able to see error message after emter incorrect password
+
+@smoke
+Scenario: Enter incorrect password should be shows error message
+	Given I open official mail ru site
+	And Click on Login button
+	When I set User Navme 'vadim.kuryan.vka' start add password
+	And Type 'IncorrectPassword' and click on login
+	Then Page shows error message that 'Password is Incorrect'
---
+++ Steps/MailRuSteps.cs
@@ -0,0 +1,52 @@
+﻿using Lesson_11_BDD.PageObjects;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Lesson_11_BDD.Steps
+{
+    [Binding]
+    public sealed class MailRuSteps
+    {
+        private readonly ScenarioContext context;
+        private HomePage homePage;
+        private LoginPage loginPage;
+
+        public MailRuSteps(ScenarioContext scenarioContext)
+        {
+            context = scenarioContext;
+        }
+
+        [Given(@"I open official mail ru site")]
+        public void OpenOfficialMailRuSite()
+        {
+            homePage = new HomePage();
+        }
+
+        [Given(@"Click on Login button")]
+        public void ClickOnLoginButton()
+        {
+            loginPage = new LoginPage();
+            homePage.GoToLogin();
+        }
+
+        [When(@"I set User Navme '(.*)' start add password")]
+        public void SetStartAddPassword(string userName)
+        {
+            loginPage.SetLogin(userName);
+        }
+
+        [When(@"Type '(.*)' and click on login")]
+        public void TypeAndClickOnLogin(string incorrectPassword)
+        {
+            loginPage.SetPassword(incorrectPassword);
+            loginPage.SignIn();
+        }
+
+        [Then(@"Page shows error message that '(.*)'")]
+        public void PageShowsErrorMessage(string errorMessage)
+        {
+            Assert.IsTrue(loginPage.errorMesage.WebElementExist(), $"Error message {errorMessage} is not appear");
+        }
+
+    }
+}
