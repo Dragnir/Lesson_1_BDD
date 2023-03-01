328785ec2b19922d1cf144ec6e4057f75ecc90a8
Vadzim Kuryan <Vadzim_Kuryan@epam.com>
2023-03-01 21:59:37 +0400

    Add API tests

---
+++ APITests/HTTPTest.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Lesson_11_BDD.APITests
+{
+    public class HTTPTest
+    {
+        [Test]
+        public void StatusCodeTest()
+        {
+            HttpWebResponse responce = MakeRequest();
+            Assert.AreEqual("OK", responce.StatusCode.ToString());
+        }
+
+        [Test]
+        public void HeaderTest()
+        {
+            HttpWebResponse responce = MakeRequest();
+            Assert.AreEqual("application/json; charset=utf-8", responce.Headers["content-type"]);
+        }
+
+        [Test]
+        public void BodyTest()
+        {
+            string responceBody = String.Empty;
+            HttpWebResponse responce = MakeRequest();
+
+            using (Stream s = responce.GetResponseStream())
+            {
+                using (StreamReader r = new StreamReader(s))
+                {
+                    responceBody = r.ReadToEnd();
+                }
+            }
+            Root jsonResult = JsonConvert.DeserializeObject<Root>(responceBody);
+            Assert.AreEqual(10, jsonResult.MyArray.Count);
+        }
+
+        public static HttpWebResponse MakeRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://jsonplaceholder.typicode.com/users");
+            request.Method = "GET";
+
+            return (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
---
+++ APITests/RestTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Lesson_11_BDD.APITests
+{
+    public class RestTest
+    {
+        [Test]
+        public void StatusCodeTest()
+        {
+            RestClient client = new RestClient("https://jsonplaceholder.typicode.com/users");
+
+            RestRequest request = new RestRequest("1", Method.Get);
+
+            RestResponse responce = client.Execute(request);
+
+            Assert.That(responce.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void HeaderTest()
+        {
+            RestClient client = new RestClient("https://jsonplaceholder.typicode.com/users");
+
+            RestRequest request = new RestRequest("1", Method.Get);
+
+            RestResponse responce = client.Execute(request);
+
+            Assert.That(responce.Headers.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void BodyTest()
+        {
+            //RestClient client = new RestClient("https://jsonplaceholder.typicode.com/users");
+
+            //RestRequest request = new RestRequest("1", Method.Get);
+
+            //RestResponse responce = client.Execute(request);
+
+            //JsonDeserializer deserialize = new JsonDeserializer();
+
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(responce);
+
+            //var result = output["name"];
+
+            //Assert.That(result, Is.EqualTo("john"), "name not correct");
+
+            //Assert.That(responce.Headers.Count, Is.EqualTo(1));
+        }
+    }
+}
